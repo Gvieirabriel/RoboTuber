6674d372a1f171c52ea6091205869e656a1f8cdd
Gvieirabriel <gvieirabriel@gmail.com>
2019-12-30 18:11:43 -0300

    Orquestrador - perguntar pelo termo de busca

---
+++ index.js
@@ -0,0 +1,14 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+    content.searchTerm = askAndReturnSearchTerm()
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a wikipedia search term:')
+    }
+    
+    console.log(content)
+}
+
+start() 
