27ca42a69445ed6e27aecc64051d99a71d36846c
Gvieirabriel <gvieirabriel@gmail.com>
2019-12-30 21:02:32 -0300

    Text Roboto - pegar e limpar conteudo, quebrar sentencas

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const readline = require('readline-sync')
+const robots = {
+    //userInput: require('./robots/user-inputs.js'),
+    text: require('./robots/text.js')
+}
 
-function start() {
-    const content = {}
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
+async function start() {
+
+    const sourceContent = {
+    }
+
+    sourceContent.searchTerm = askAndReturnSearchTerm()
+    sourceContent.prefix = askAndReturnPrefix()
+
+    await robots.text(sourceContent)
 
     function askAndReturnSearchTerm() {
-        return readline.question('Type a wikipedia search term: ')
+        return readline.question('Type a source search term: ')
     }
 
     function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes)
         const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
+
         return selectedPrefixText;
     }
 
-    console.log(content)
+    console.log(sourceContent)
 }
 
 start() 
